957531004e3b866ceee56983c6eea07201cb150f
Diego Benavides <az4diegoz@gmail.com>
2022-08-13 23:26:14 -0500

    Initial Commit: index.js: se ha creado event listeners para poder manipular el dom. task.js: se creo el getter taskInfo y se modifico el constructor para que al crear un task se agregue su taskInfo al conjunto allTasks.

--- index.js
+++ index.js
@@ -1,56 +1,38 @@
 import { HoursBlocks } from "./src/hours_blocks.js";
 import { Day } from "./src/day.js";
 import { Week } from "./src/week.js"; 
-import { Task, taskTypesAdd, getTypeTask } from "./src/task.js"; 
+import { Task, taskTypesAdd, getTypeTask, allTasks, deleteTaskFromAllTasks} from "./src/task.js"; 
 
 
-function addTaskTest(week) {
-    const firstMessage = 'Now, enter some tasks'
-    alert(firstMessage)
-    let keepLoging = true
-    while (keepLoging) {
-        const newTaskMessage = 'Lets create a new Task!'
-        alert(newTaskMessage)
-        const newUserTaskName = prompt('Type your task name')
-        const newUserTaskType = prompt('Type your task type')
-        const newUserTaskColor = prompt('Type your task color')
+let newTaskForm = document.forms['NewTaskForm']
+let tasksList = document.getElementById('TasksList')
 
-        const addTaskMessage = 'Now add your task the week'
-        alert(addTaskMessage)
-        const taskDay = prompt('Type the task day')
-        const taskBeginningHour = prompt('Type task beginning hour |24 hour format')
-        const taskEndingHour = prompt('Type task ending hour|24 hour format')
-
-        const newUserTask = new Task(newUserTaskName, newUserTaskType, newUserTaskColor)
-        week.scheduleTask(taskDay, newUserTask, taskBeginningHour, taskEndingHour)
-        const userPrompt = prompt('Do you wanna enter a new task? Y|N')
-        keepLoging = userPrompt == 'Y' ? true : false
-    }
-    return 'Succeed'
+function addTask() {
+    let taskName = document.getElementById("TaskName").value;
+    let taskType = document.getElementById("TaskType").value;
+    let taskColor = document.getElementById("TaskColor").value;
+    new Task(taskName, taskType, taskColor)
+    newTaskForm.reset();
 }
 
+function deleteTask() {
+    let taskName = document.getElementById("TaskName").value;
+    let taskType = document.getElementById("TaskType").value;
+    let taskColor = document.getElementById("TaskColor").value;
+    let newTask = new Task(taskName, taskType, taskColor)
+    deleteTaskFromAllTasks(newTask)
+    newTaskForm.reset();
+}
 
-
-const header = 
-`
-*********************************************
-        Welcome to Easy ScheduleMaker
-*********************************************
-`
-alert(header)
-const weekTimeBeginning = prompt('Lets create a new week! Type the beginning hour| Use 24 hour format, example: "6:30" or "5:00", only use half hours or whole hours')
-const weekTimeEnding = prompt('Lets create a new week! Type the ending hour| Use 24 hour format, example: "16:30" or "20:00", only use half hours or whole hours')
-const userWeek =  new Week(weekTimeBeginning, weekTimeEnding)
-
-console.log("Creation of the user's week")
-console.log(userWeek)
-
-console.log(addTaskTest(userWeek))
-
-alert('You end the Easy SheduleMaker pre-alpha test, take a look in your console to see the week')
-console.log('Your week schedule')
-console.log(userWeek)
-console.log('Add Tasks, classified by their types \n')
-console.log(taskTypesAdd)
-console.log('Week Tasks')
-console.log(userWeek.weekTasks)
+function showTasks() {
+    tasksList.innerHTML = ""
+    for (const task of allTasks) {
+        const taskLi = document.createElement("li");
+        taskLi.innerHTML = task
+        tasksList.appendChild(taskLi);
+    }
+}
+document.getElementById("AddTask").addEventListener("click", addTask)
+document.getElementById("DeleteTask").addEventListener("click", deleteTask)
+document.getElementById("ShowTasks").addEventListener("click", showTasks)
+console.log(allTasks)
--- src/task.js
+++ src/task.js
@@ -10,17 +10,31 @@ const taskTypesAdd = {
     home: new Set()
 }
 
+const allTasks = new Set()
+
 function getTypeTask(taskType) {
     return taskTypesAdd[taskType]
+}
+
+function deleteTaskFromAllTasks(task) { // función provisional
+    if (allTasks.has(task.taskInfo)) {
+        allTasks.delete(task.taskInfo)
+    }
 }
 
 class Task {
     constructor(taskName, taskType, taskColour) {
         this._taskName = taskName;
         this._taskType = taskType;
-        this._taskColour = taskColours[taskColour];
+        this._taskColour = taskColour;
         taskTypesAdd[taskType] ? taskTypesAdd[taskType].add(this) : taskTypesAdd[taskType] = new Set().add(this);
+        allTasks.add(this.taskInfo)
+    }
+
+    get taskInfo() {
+        const taskInfo = `Task Name: ${this._taskName}; Task Type: ${this._taskType}`
+        return taskInfo
     }
 }
 
-export {Task, taskTypesAdd, getTypeTask};
+export {Task, taskTypesAdd, getTypeTask, allTasks, deleteTaskFromAllTasks};
